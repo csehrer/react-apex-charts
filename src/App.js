ca1ba2dbd253182cd4a15413da1f542aee9af7cd
csehrer <cedric.sehrer@yahoo.com>
2020-01-27 13:10:30 +0300

    UPDATE: headerbar updated!

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ApexChart from './components/charts/apexchart'
-import PrimarySearchAppBar from './layouts/headerbar'
+import HeaderBar from './layouts/headerbar'
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <HeaderBar></HeaderBar>
         <ApexChart></ApexChart>
       </div>
     );
